9777fd10cb0105425e09cb2dd413fe09ce263ca3
rohit-va <rohitvijathani@gmail.com>
2015-12-02 15:55:30 +0530

    incorrect regex program

--- regex/hackerrank/incorrect_regex.py
+++ regex/hackerrank/incorrect_regex.py
@@ -1,1 +1,8 @@
 __author__ = 'rohitat'
+import re
+for _ in xrange(int(raw_input())):
+    match = re.search(r'[\*\+]{0,1}',raw_input())
+    if(match):
+        print "True"
+    else:
+        print "False"
